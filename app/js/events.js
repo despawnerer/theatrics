e028e114818c3c1b81000b3a39698812f01af502
Алексей Шельф <despawn@gmail.com>
2015-10-15 12:02:59 +0300

    Put the link around both item image and item header

--- app/js/events.js
+++ app/js/events.js
@@ -7,16 +7,14 @@ import {capfirst} from './utils';
 
 const itemTemplate = (item) => `
 <li class="list-item">
-  <div class="list-image-container">
-    <a href="/event/${item.id}/">
+  <a href="/event/${item.id}/">
+    <div class="list-image-container">
       <img data-src="${item.images[0].thumbnails['640x384']}" class="list-image lazyload"/>
-    </a>
-  </div>
-  <h2 class="item-header">
-    <a href="/event/${item.id}/">
+    </div>
+    <h2 class="item-header">
       ${capfirst(item.short_title || item.title)}
-    </a>
-  </h2>
+    </h2>
+  </a>
   <div class="tagline">${item.tagline}</div>
   <div class="place">
     ${item.place ? capfirst(item.place.title) : ''}

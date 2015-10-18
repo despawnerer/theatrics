a518cfa0f8f5e1c66ea678025b696442c95b3177
Алексей Шельф <despawn@gmail.com>
2015-10-19 00:14:18 +0300

    Properly serve the client from all possible URLs

--- server/app/app.py
+++ server/app/app.py
@@ -1,11 +1,13 @@
 from aiohttp import web
 
-from .handlers import handle_api
+from .handlers import serve_api, serve_client
 from .settings import CLIENT_DIR, DEBUG
 
 
 app = web.Application()
-app.router.add_route('GET', '/api/{path:.+}/', handle_api)
+app.router.add_route('GET', '/api/{path:.+}/', serve_api)
 
 if DEBUG:
-    app.router.add_static('/', CLIENT_DIR)
+    app.router.add_static('/static/', CLIENT_DIR)
+    app.router.add_route('GET', '/', serve_client)
+    app.router.add_route('GET', '/{path:.+}/', serve_client)
--- server/app/handlers.py
+++ server/app/handlers.py
@@ -1,11 +1,12 @@
+import os
 import aiohttp
 import ujson
 from aiohttp import web
 
-from .settings import DEBUG, KUDAGO_API_BASE_URL
+from .settings import DEBUG, KUDAGO_API_BASE_URL, CLIENT_DIR
 
 
-async def handle_api(request):
+async def serve_api(request):
     url = '{}/{}/?{}'.format(
         KUDAGO_API_BASE_URL,
         request.match_info['path'],
@@ -21,3 +22,23 @@ async def handle_api(request):
                 body[field] = value.replace(KUDAGO_API_BASE_URL, '/api')
 
     return web.Response(body=ujson.dumps(body).encode('utf-8'))
+
+
+async def serve_client(request):
+    filepath = os.path.join(CLIENT_DIR, 'index.html')
+    stat = os.stat(filepath)
+    chunk_size = 256 * 1024
+
+    response = web.StreamResponse()
+    response.content_type = 'text/html'
+    response.last_modified = stat.st_mtime
+    response.content_length = stat.st_size
+
+    response.start(request)
+    with open(filepath, 'rb') as f:
+        chunk = f.read(chunk_size)
+        while chunk:
+            response.write(chunk)
+            chunk = f.read(chunk_size)
+
+    return response

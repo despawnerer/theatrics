410dca3e09f5e8ef6fed7c7424e2e9156846fdbb
Алексей Шельф <despawn@gmail.com>
2015-12-05 14:41:09 +0300

    Don't request age restriction, we don't really need it

--- client/src/js/views/events-view.js
+++ client/src/js/views/events-view.js
@@ -17,7 +17,7 @@ export default class EventsView extends View {
     this.feed = new Feed(
       '/events/', {
         categories: 'theater,-kids',
-        fields: 'place,images,tagline,id,age_restriction,title,short_title',
+        fields: 'place,images,tagline,id,title,short_title,categories',
         expand: 'place,images',
         page_size: 24,
       });

4c5808d6c1dce0cedfd773d26f365da89e35bff0
Алексей Шельф <despawn@gmail.com>
2015-12-14 18:46:22 +0300

    Babelify gulpfile and move babel options to package.json
    Fixes #11

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -1,32 +1,35 @@
-var gulp = require('gulp');
-var util = require('gulp-util');
+'use strict';
 
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
+import gulp from 'gulp';
+import util from 'gulp-util';
 
-var browserify = require('browserify');
-var watchify = require('watchify');
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
 
-var postcss = require('gulp-postcss');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var template = require('gulp-template');
+import csswring from 'csswring';
+import autoprefixer  from 'autoprefixer';
+import postcssImport from 'postcss-import';
+import postcssAssets from 'postcss-assets';
 
+import browserify from 'browserify';
+import watchify from 'watchify';
 
-function logError(e) {
-  util.log(util.colors.red('Error'), e);
-}
+import postcss from 'gulp-postcss';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import template from 'gulp-template';
 
 
 /* CSS */
 
-gulp.task('build-css', function() {
-  return gulp.src('src/css/index.css')
+gulp.task('build-css', () => {
+  return gulp
+    .src('src/css/index.css')
     .on('error', logError)
     .pipe(postcss([
-      require('postcss-import')(),
-      require('autoprefixer')(),
-      require('postcss-assets')({
+      postcssImport,
+      autoprefixer,
+      postcssAssets({
         loadPaths: ['src']
       }),
     ]))
@@ -34,15 +37,16 @@ gulp.task('build-css', function() {
 });
 
 
-gulp.task('watch-css', function () {
+gulp.task('watch-css', () => {
   gulp.start(['build-css']);
   gulp.watch('src/css/**/*.css', ['build-css']);
 });
 
-gulp.task('build-min-css', ['build-css'], function () {
-  return gulp.src(['build/*.css', '!build/*.min.css'])
+gulp.task('build-min-css', ['build-css'], () => {
+  return gulp
+    .src(['build/*.css', '!build/*.min.css'])
     .pipe(postcss([
-      require('csswring')()
+      csswring,
     ]))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build'));
@@ -55,7 +59,7 @@ const JS_ENTRIES = ['src/js/index.js'];
 const JS_TARGET_FILENAME = 'index.js';
 
 function buildJS(options) {
-  var b = browserify({
+  const b = browserify({
     entries: JS_ENTRIES,
     debug: options.debug,
 
@@ -65,8 +69,9 @@ function buildJS(options) {
     packageCache: {}
   });
 
-  var buildBundle = function() {
-    var stream = b.bundle()
+  const buildBundle = () => {
+    return b
+      .bundle()
       .on('error', logError)
       .pipe(source(JS_TARGET_FILENAME))
       .pipe(buffer())
@@ -80,21 +85,22 @@ function buildJS(options) {
   return buildBundle();
 }
 
-gulp.task('build-js', function () {
+gulp.task('build-js', () => {
   return buildJS({
     debug: true,
   });
 });
 
-gulp.task('watch-js', function () {
+gulp.task('watch-js', () => {
   buildJS({
     debug: true,
     watch: true,
   });
 });
 
-gulp.task('build-min-js', ['build-js'], function () {
-  return gulp.src('build/index.js')
+gulp.task('build-min-js', ['build-js'], () => {
+  return gulp
+    .src('build/index.js')
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build'));
@@ -103,29 +109,31 @@ gulp.task('build-min-js', ['build-js'], function () {
 
 /* HTML */
 
-gulp.task('build-html', function() {
-  var context = {
+gulp.task('build-html', () => {
+  const context = {
     min: false,
     buildDate: new Date(),
   }
-  return gulp.src('src/*.ejs')
+  return gulp
+    .src('src/*.ejs')
     .on('error', logError)
     .pipe(template(context))
     .pipe(rename({extname: '.html'}))
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('watch-html', function() {
+gulp.task('watch-html', () => {
   gulp.start(['build-html']);
   gulp.watch('src/*.ejs', ['build-html']);
 });
 
-gulp.task('build-min-html', function() {
-  var context = {
+gulp.task('build-min-html', () => {
+  const context = {
     min: true,
     buildDate: new Date(),
   }
-  return gulp.src('src/*.ejs')
+  return gulp
+    .src('src/*.ejs')
     .on('error', logError)
     .pipe(template(context))
     .pipe(rename({extname: '.html'}))
@@ -135,11 +143,13 @@ gulp.task('build-min-html', function() {
 
 /* Fonts */
 
-gulp.task('build-fonts', function() {
-  return gulp.src('src/fonts/*').pipe(gulp.dest('build/fonts'));
+gulp.task('build-fonts', () => {
+  return gulp
+    .src('src/fonts/*')
+    .pipe(gulp.dest('build/fonts'));
 });
 
-gulp.task('watch-fonts', function() {
+gulp.task('watch-fonts', () => {
   gulp.start(['build-fonts']);
   gulp.watch('src/fonts/*', ['build-fonts']);
 });
@@ -147,13 +157,30 @@ gulp.task('watch-fonts', function() {
 
 /* Big tasks */
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.start('watch-css');
   gulp.start('watch-js');
   gulp.start('watch-html');
   gulp.start('watch-fonts');
 });
 
-gulp.task('build', ['build-css', 'build-js', 'build-html', 'build-fonts']);
+gulp.task('build', [
+  'build-css',
+  'build-js',
+  'build-html',
+  'build-fonts',
+]);
+
 gulp.task('build-min', [
-  'build-min-css', 'build-min-js', 'build-min-html', 'build-fonts']);
+  'build-min-css',
+  'build-min-js',
+  'build-min-html',
+  'build-fonts'
+]);
+
+
+/* Utils */
+
+function logError(e) {
+  util.log(util.colors.red('Error'), e);
+}

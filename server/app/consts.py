5dec21781415a6e071e6ea030240d591ef0e3864
Алексей Шельф <despawn@gmail.com>
2016-01-02 16:41:37 +0300

    Cache API responses for 15 minutes
    There’s really no need to be constantly fetching stuff

--- server/app/consts.py
+++ server/app/consts.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -6,3 +7,6 @@ SERVER_DIR = os.path.dirname(THIS_DIR)
 CLIENT_DIR = os.path.join(os.path.dirname(SERVER_DIR), 'client/build')
 
 KUDAGO_API_BASE_URL = 'http://kudago.com/public-api/v1'
+
+RESPONSE_CACHE_INTERVAL = timedelta(minutes=15)
+RESPONSE_CACHE_SECONDS = int(RESPONSE_CACHE_INTERVAL.total_seconds())
--- server/app/handlers.py
+++ server/app/handlers.py
@@ -1,9 +1,16 @@
 import os
 import aiohttp
 import json
-from aiohttp import web
+import time
+from datetime import datetime
+from aiohttp import web, hdrs
 
-from .consts import KUDAGO_API_BASE_URL, CLIENT_DIR
+from .consts import (
+    CLIENT_DIR,
+    KUDAGO_API_BASE_URL,
+    RESPONSE_CACHE_INTERVAL,
+    RESPONSE_CACHE_SECONDS,
+)
 
 
 async def serve_api(request):
@@ -21,9 +28,14 @@ async def serve_api(request):
             if value:
                 body[field] = value.replace(KUDAGO_API_BASE_URL, '/api')
 
+    expire_dt = datetime.utcnow() + RESPONSE_CACHE_INTERVAL
     return web.Response(
         text=json.dumps(body, ensure_ascii=False),
-        content_type='application/json'
+        content_type='application/json',
+        headers={
+            hdrs.CACHE_CONTROL: 'public, max-age=%d' % RESPONSE_CACHE_SECONDS,
+            hdrs.EXPIRES: expire_dt.strftime('%a, %d %b %Y %H:%M:%S GMT'),
+        }
     )
 
 

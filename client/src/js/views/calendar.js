b5d40fdd8a34243f5ac7a5eec445cd5bb85037bd
Алексей Шельф <despawn@gmail.com>
2016-01-04 13:12:00 +0300

    Lowercase days of week in the calendar

--- client/src/js/views/calendar.js
+++ client/src/js/views/calendar.js
@@ -50,7 +50,7 @@ export default class Calendar extends View {
     element.setAttribute('class', 'calendar-day');
     element.setAttribute('data-date', dateString);
     element.innerHTML = `
-      <a href="${target}">${day.format('D')}<br/>${day.format('dd')}</a>`;
+      <a href="${target}">${day.format('D')}<br/>${day.format('dd').toLowerCase()}</a>`;
     return element;
   }
 

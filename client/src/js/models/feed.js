717d6cf4061d2d50f2836c2d297eaea6ae6fca78
Алексей Шельф <despawn@gmail.com>
2016-01-09 13:03:25 +0300

    Return Promises from Feed's fetch methods

--- client/src/js/models/feed.js
+++ client/src/js/models/feed.js
@@ -27,33 +27,38 @@ export default class Feed extends EventEmitter {
     let items = null;
     let nextURL = null;
 
-    const hasMore = () => nextURL !== null || items == null;
-    const fetchNextPage = () => {
-      if (!hasMore()) {
-        this._handleLoaded(loadingQuery, items, items, nextURL);
-        return;
-      }
-
-      this._doFetchMore(loadingQuery, nextURL).then(response => {
-        if (!loadingQuery.equals(this.query)) {
+    return new Promise((resolve, reject) => {
+      const hasMore = () => nextURL !== null || items == null;
+      const fetchNextPage = () => {
+        if (!hasMore()) {
+          this._handleLoaded(loadingQuery, items, items, nextURL);
+          resolve(items);
           return;
         }
-        items = (items || []).concat(response.data.results);
-        nextURL = response.data.next;
-        fetchNextPage();
-      });
-    }
 
-    fetchNextPage();
+        this._doFetchMore(loadingQuery, nextURL).then(response => {
+          if (!loadingQuery.equals(this.query)) {
+            reject();
+            return;
+          }
+          items = (items || []).concat(response.data.results);
+          nextURL = response.data.next;
+          fetchNextPage();
+        });
+      }
+
+      fetchNextPage();
+    });
   }
 
   fetchMore() {
     const loadingQuery = this.query.clone();
-    this._doFetchMore(this.query, this.nextURL).then(response => {
+    return this._doFetchMore(this.query, this.nextURL).then(response => {
       const newItems = response.data.results;
       const allItems = (this.items || []).concat(newItems);
       const nextURL = response.data.next;
       this._handleLoaded(loadingQuery, newItems, allItems, nextURL);
+      return newItems;
     });
   }
 

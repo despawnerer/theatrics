6b6c48fe3fe4288ecf8f0ffc06feee21f81b10d5
Aleksei Voronov <despawn@gmail.com>
2016-03-04 19:01:17 +0300

    Add fastclick extension to increase responsiveness on iDevices

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,7 +4,9 @@ import 'core-js/fn/symbol';
 import 'core-js/fn/array';
 import 'core-js/fn/string/ends-with';
 import 'core-js/fn/string/starts-with';
+
 import 'whatwg-fetch';
+import attachFastClick from 'fastclick';
 import raf from 'raf';
 
 import 'moment-timezone/moment-timezone';
@@ -15,8 +17,9 @@ import moment from 'moment';
 import timezones from '../data/timezones.json';
 import App from './core/app';
 
-moment.tz.load(timezones);
 raf.polyfill();
+attachFastClick(document.documentElement);
+moment.tz.load(timezones);
 
 const app = new App();
 app.run();

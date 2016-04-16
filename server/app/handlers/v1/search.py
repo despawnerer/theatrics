e3f244e40c39052b97b773954efa035695445140
Aleksei Voronov <despawn@gmail.com>
2016-04-16 11:55:37 +0300

    Add expand support to search

--- server/app/handlers/v1/search.py
+++ server/app/handlers/v1/search.py
@@ -3,6 +3,7 @@ from marshmallow import Schema, fields
 from ..helpers import list_handler, with_params
 
 from .score import get_default_score_functions
+from .events import EXPANDABLE_RELATIONS
 
 
 __all__ = ['search']
@@ -13,7 +14,7 @@ class SearchParams(Schema):
     location = fields.String()
 
 
-@list_handler()
+@list_handler(relations=EXPANDABLE_RELATIONS)
 @with_params(SearchParams)
 async def search(request, q, location=None):
     filters = []

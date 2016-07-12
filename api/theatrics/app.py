b9adf501d0abf256c686e560c44b810a9c1bdbb0
Aleksei Voronov <despawn@gmail.com>
2016-07-12 15:14:05 +0300

    Remove API prefix from the API app
    Use the correct proxy_pass directive in nginx

--- api/theatrics/app.py
+++ api/theatrics/app.py
@@ -5,8 +5,8 @@ from .handlers import v1
 
 app = web.Application()
 
-app.router.add_route('GET', '/api/v1/events/', v1.event_list)
-app.router.add_route('GET', '/api/v1/events/{id:\d+}/', v1.event)
-app.router.add_route('GET', '/api/v1/places/', v1.place_list)
-app.router.add_route('GET', '/api/v1/places/{id:\d+}/', v1.place)
-app.router.add_route('GET', '/api/v1/search/', v1.search)
+app.router.add_route('GET', '/v1/events/', v1.event_list)
+app.router.add_route('GET', '/v1/events/{id:\d+}/', v1.event)
+app.router.add_route('GET', '/v1/places/', v1.place_list)
+app.router.add_route('GET', '/v1/places/{id:\d+}/', v1.place)
+app.router.add_route('GET', '/v1/search/', v1.search)

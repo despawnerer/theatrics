950155d6b061f555c19fb33fc1f5336507f610d8
Aleksei Voronov <despawn@gmail.com>
2016-07-28 14:21:48 +0300

    Make print_progress handle empty iterables

--- importer/importer/utils.py
+++ importer/importer/utils.py
@@ -80,11 +80,12 @@ def get_today():
 
 
 def print_progress(iterable, message, every=100):
+    count = 0
     for count, item in enumerate(iterable, 1):
         yield item
         if count % every == 0:
             print(message % count)
-    if count % every != 0:
+    if count % every != 0 or count == 0:
         print(message % count)
 
 

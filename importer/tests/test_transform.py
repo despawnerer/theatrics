3a1f35922260dfae59406b20c23e3342a6e4f4f9
Aleksei Voronov <despawn@gmail.com>
2016-08-01 15:10:17 +0300

    Add thorough tests for date splitting

--- importer/tests/test_transform.py
+++ importer/tests/test_transform.py
@@ -4,7 +4,43 @@ from importer.transform import split_date
 
 
 class SplitDateTestCase(TestCase):
-    def test_split_single_with_explicit_end(self):
+    def test_continuous(self):
+        spec = {
+            'start_date': '2016-08-01',
+            'start_time': '19:00:00',
+            'end_date': '2016-08-14',
+            'end_time': '10:00:00',
+            'is_continuous': True,
+            'schedules': [],
+        }
+        self.assertCountEqual(split_date(spec), [
+            {
+                'start_date': '2016-08-01',
+                'start_time': '19:00:00',
+                'end_date': '2016-08-14',
+                'end_time': '10:00:00',
+                'is_continuous': True,
+            }
+        ])
+
+    def test_single_day_with_no_end_date(self):
+        spec = {
+            'start_date': '2014-04-27',
+            'start_time': '12:30:00',
+            'end_date': None,
+            'end_time': '13:10:00',
+            'is_continuous': False,
+            'schedules': [],
+        }
+        self.assertCountEqual(split_date(spec), [{
+            'start_date': '2014-04-27',
+            'start_time': '12:30:00',
+            'end_date': None,
+            'end_time': '13:10:00',
+            'is_continuous': False,
+        }])
+
+    def test_single_day_with_end_date(self):
         spec = {
             'start_date': '2014-04-27',
             'start_time': '12:30:00',
@@ -13,12 +49,89 @@ class SplitDateTestCase(TestCase):
             'is_continuous': False,
             'schedules': [],
         }
-        split_specs = list(split_date(spec))
-        self.assertEqual(len(split_specs), 1)
-        self.assertEqual(split_specs[0], {
+        self.assertCountEqual(split_date(spec), [{
             'start_date': '2014-04-27',
             'start_time': '12:30:00',
             'end_date': '2014-04-27',
             'end_time': '13:10:00',
             'is_continuous': False,
-        })
+        }])
+
+    def test_multi_day_without_schedule(self):
+        spec = {
+            'start_date': '2014-04-27',
+            'start_time': '12:30:00',
+            'end_date': '2014-04-29',
+            'end_time': '13:10:00',
+            'is_continuous': False,
+            'schedules': [],
+        }
+        self.assertCountEqual(split_date(spec), [
+            {
+                'start_date': '2014-04-27',
+                'start_time': '12:30:00',
+                'end_date': '2014-04-27',
+                'end_time': '13:10:00',
+                'is_continuous': False,
+            },
+            {
+                'start_date': '2014-04-28',
+                'start_time': '12:30:00',
+                'end_date': '2014-04-28',
+                'end_time': '13:10:00',
+                'is_continuous': False,
+            },
+            {
+                'start_date': '2014-04-29',
+                'start_time': '12:30:00',
+                'end_date': '2014-04-29',
+                'end_time': '13:10:00',
+                'is_continuous': False,
+            },
+        ])
+
+    def test_multi_day_with_schedule(self):
+        spec = {
+            'start_date': '2016-08-01',
+            'start_time': None,
+            'end_date': '2016-08-14',
+            'end_time': None,
+            'is_continuous': False,
+            'schedules': [
+                {
+                    'days_of_week': [0, 6],
+                    'start_time': '14:00:00',
+                    'end_time': None,
+                }
+            ],
+        }
+        self.assertCountEqual(split_date(spec), [
+            {
+                'start_date': '2016-08-01',
+                'start_time': '14:00:00',
+                'end_date': '2016-08-01',
+                'end_time': None,
+                'is_continuous': False,
+            },
+            {
+                'start_date': '2016-08-07',
+                'start_time': '14:00:00',
+                'end_date': '2016-08-07',
+                'end_time': None,
+                'is_continuous': False,
+            },
+            {
+                'start_date': '2016-08-08',
+                'start_time': '14:00:00',
+                'end_date': '2016-08-08',
+                'end_time': None,
+                'is_continuous': False,
+            },
+            {
+                'start_date': '2016-08-14',
+                'start_time': '14:00:00',
+                'end_date': '2016-08-14',
+                'end_time': None,
+                'is_continuous': False,
+            },
+        ])

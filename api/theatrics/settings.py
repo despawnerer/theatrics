d94a9e20c96b2220001b63441491e9b4dfc06110
Aleksei Voronov <despawn@gmail.com>
2016-08-02 12:45:35 +0300

    With aides 0.5 we don't need to unprefix things

--- api/theatrics/settings.py
+++ api/theatrics/settings.py
@@ -1,14 +1,5 @@
 import os
 
 
-def unprefix(s, prefix):
-    return s[len(prefix):] if s.startswith(prefix) else s
-
-
-ELASTICSEARCH_ENDPOINTS = [
-    unprefix(
-        os.environ.get('ELASTICSEARCH_URL', 'elasticsearch:9200'),
-        'http://'
-    )
-]
+ELASTICSEARCH_ENDPOINTS = [os.environ.get('ELASTICSEARCH_URL', 'elasticsearch:9200')]
 ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX', 'theatrics')
